1f1385bc68a38be8e3af4e5aac4eff907b6eb4d8
StaceyP <phoenixdoll@gmail.com>
2024-07-05 12:06:49 +1000

    added console things ONLY for tetsing

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log("Website loaded");
+
+    var audio = document.querySelector('#latest');
+
+    audio.addEventListener('play', function () {
+        console.log('Audio played');
+        // Perform actions when the audio starts playing
+    });
+
+    audio.addEventListener('pause', function () {
+        console.log('Audio paused');
+        // Perform actions when the audio is paused
+    });
+
+    audio.addEventListener('ended', function () {
+        console.log('Audio ended');
+        // Perform actions when the audio finishes playing
+    });
 });
