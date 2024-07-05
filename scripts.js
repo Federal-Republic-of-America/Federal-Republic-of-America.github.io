8f755e871629506f3669549c65a6fe737541d0b0
StaceyP <phoenixdoll@gmail.com>
2024-07-05 11:33:54 +1000

    attempt at interactivity wtih flag gif

--- scripts.js
+++ scripts.js
@@ -1,4 +1,22 @@
 // Example JavaScript to add interactivity (optional)
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     console.log("Website loaded");
+    var audio = document.getElementById('audio');
+    var animatedGif = document.getElementById('animatedGif');
+    var staticGif = document.getElementById('staticGif');
+
+    audio.addEventListener('play', function () {
+        animatedGif.style.display = 'block';
+        staticGif.style.display = 'none';
+    });
+
+    audio.addEventListener('pause', function () {
+        animatedGif.style.display = 'none';
+        staticGif.style.display = 'block';
+    });
+
+    audio.addEventListener('ended', function () {
+        animatedGif.style.display = 'none';
+        staticGif.style.display = 'block';
+    });
 });
