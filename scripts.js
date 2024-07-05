4bd352c311726e500d7646a8635167923531afd3
StaceyP <phoenixdoll@gmail.com>
2024-07-05 11:38:49 +1000

    console logs

--- scripts.js
+++ scripts.js
@@ -6,16 +6,19 @@ document.addEventListener('DOMContentLoaded', function () {
     var staticGif = document.getElementById('staticGif');
 
     audio.addEventListener('play', function () {
+        console.log('Audio is playing');
         animatedGif.style.display = 'block';
         staticGif.style.display = 'none';
     });
 
     audio.addEventListener('pause', function () {
+        console.log('Audio is paused');
         animatedGif.style.display = 'none';
         staticGif.style.display = 'block';
     });
 
     audio.addEventListener('ended', function () {
+        console.log('Audio has ended');
         animatedGif.style.display = 'none';
         staticGif.style.display = 'block';
     });
